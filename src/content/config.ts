57e3f98d99d4a31e4a019df410d260dd1f636ee5
firefly <promptsiren@gmail.com>
2023-12-10 21:12:35 -0500

    categories and author filtering

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,37 @@
-// Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
-// Define a `type` and `schema` for each collection
-const postsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-      title: z.string(),
-      pubDate: z.date(),
-      description: z.string(),
-      author: z.string(),
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
-      tags: z.array(z.string())
-    })
-});
-// Export a single `collections` object to register your collection(s)
+import { z, defineCollection, reference } from "astro:content";
+
 export const collections = {
-  posts: postsCollection,
+  posts: defineCollection({
+    type: "content",
+    schema: z.object({
+      title: z.string().max(60, {
+        message: "Title must be 60 characters or less.",
+      }),
+      description: z.string().max(160, {
+        message: "Description must be 160 characters or less.",
+      }),
+      date: z.date(),
+      cover: z.object({
+        src: z.string(),
+        alt: z.string(),
+      }),
+      author: reference("team"),
+      category: z.string().default("General"),
+      tags: z.array(z.string()).optional(),
+      relatedPosts: z.array(reference("posts")).optional(),
+      draft: z.boolean().optional(),
+    }),
+  }),
+  team: defineCollection({
+    type: "data",
+    schema: ({ image }) =>
+      z.object({
+        name: z.string(),
+        bio: z.string(),
+        email: z.string(),
+        role: z.enum(["Software", "Design", "Marketing"]),
+        headshot: image(),
+      }),
+  }),
+ 
 };
