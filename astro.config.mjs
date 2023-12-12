346edc769785cc0c426eb36a9caa8897d2d7365e
firefly <promptsiren@gmail.com>
2023-12-11 22:21:27 -0500

    added giscus for comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import svelte from "@astrojs/svelte";
+
 
 // https://astro.build/config
 export default defineConfig({
