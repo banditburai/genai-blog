6e59f04da308c898b197eeb1abb07763ba27816d
firefly <promptsiren@gmail.com>
2023-12-14 17:40:17 -0500

    sentry test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,18 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 
+import sentry from "@sentry/astro";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), svelte()]
+  integrations: [tailwind(), mdx(), svelte(), 
+    sentry(
+      {
+        dsn: "https://ae18d6deff77e66b880cbf009a4d015e@o4506305828290560.ingest.sentry.io/4506396468641792",
+        sourceMapsUploadOptions: {
+          project: "genai-blog",
+          authToken: process.env.SENTRY_AUTH_TOKEN,
+        },
+      }
+    )]
 });
