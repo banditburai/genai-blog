0bfad3a1243ab4c9b8ab992bb76dc00968bc8295
firefly <promptsiren@gmail.com>
2023-12-14 19:04:07 -0500

    changed site in robots and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://genai-blog-banditburai.vercel.app/',
+  site: 'https://genai-blog.vercel.app/',
   integrations: [tailwind(), mdx(), svelte(), sentry({
     dsn: "https://ae18d6deff77e66b880cbf009a4d015e@o4506305828290560.ingest.sentry.io/4506396468641792",
     sourceMapsUploadOptions: {
